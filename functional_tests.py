b458d755f1d3b1bb89bcb2fa0ff4058db8bde858
MLazz <michele.lazzeri@studenti.unimi.it>
2016-07-27 16:25:28 +0200

    Gigi

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -23,22 +28,20 @@ class NewVisitorTest(unittest.TestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
-        input_box.send_keys('Buy peacock feathers')
+        text1 = 'Buy peacock feathers'
+        input_box.send_keys(text1)
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: ' + text1)
 
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
-        
-        input_box.send_keys('Use peacock feathers to make a fly')
+
+        text2 = 'Use peacock feathers to make a fly';
+        input_box.send_keys(text2)
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('2: ' + text2)
 
         self.fail('Finish')
 
