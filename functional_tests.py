638c31204fe9c252862d3a84235dee919769b814
MLazz <michele.lazzeri@studenti.unimi.it>
2016-07-27 16:21:34 +0200

    Gigi

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
 import unittest
 
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
@@ -24,14 +24,29 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         input_box.send_keys('Buy peacock feathers')
-        input_box.send_keys(Keys.Enter)
+        input_box.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+        
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish')
 
+
+if __name__ == '__main__':
+    unittest.main()
+
+    self.fail('Finish')
 
 if __name__ == '__main__':
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
+
 from lists.views import home_page
 
 
@@ -11,8 +12,17 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest
+        request = HttpRequest()
         response = home_page(request)
 
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_post_req(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST["item_text"] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
         self.assertEqual(response.content.decode(), expected_html)
