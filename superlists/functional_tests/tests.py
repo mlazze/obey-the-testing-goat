56ad8cfbf120b1f030c8128a6ffb7cc2d5938b56
MLazz <michele.lazzeri@studenti.unimi.it>
2016-07-31 16:17:50 +0200

    new URL + view for adding to existing lists. FT passes :)

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -65,6 +65,6 @@ class NewVisitorTest(LiveServerTestCase):
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, r'/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
-        page_text = self.browser.find_element_by_tag_name('body')
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn(text1, page_text)
         self.assertIn(text3, page_text)
