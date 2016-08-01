0c6fa2329930c1875f4f6713641c9a0ba0feacd5
MLazz <michele.lazzeri@studenti.unimi.it>
2016-08-01 17:15:46 +0200

    Moved fTs into their own individual files

--- source/functional_tests/test_simple_list_creation.py
+++ source/functional_tests/test_simple_list_creation.py
@@ -1,40 +1,11 @@
-import sys
-
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 
+from functional_tests.base import FunctionalTest
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
 
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-
-    def setUp(self):
-        self.browser = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
-                                        desired_capabilities=DesiredCapabilities.CHROME)
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.server_url)
 
@@ -84,13 +55,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn(text1, page_text)
         self.assertIn(text3, page_text)
-
-    def test_layout_and_styling(self):
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location["x"] + inputbox.size['width'] / 2, 512, delta=10)
-        inputbox.send_keys('testing\n')
-        self.browser.find_element_by_id('id_new_item')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location["x"] + inputbox.size['width'] / 2, 512, delta=10)
