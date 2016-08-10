aee208d8cd6250e2403d8de19c2e990a02aa0bdd
MLazz <michele.lazzeri@studenti.unimi.it>
2016-08-10 18:37:46 +0200

    Added client side js for persona login

--- source/accounts/static/accounts/accounts.js
+++ source/accounts/static/accounts/accounts.js
@@ -1,6 +1,23 @@
-var initialize = function (navigator) {
+var initialize = function (navigator, user, token, urls) {
     $('#id_login').on('click', function () {
         navigator.id.request();
+    });
+    navigator.id.watch({
+        loggedInUser: user,
+        onlogin: function (assertion) {
+            $.post(
+                urls.login,
+                {
+                    assertion: assertion,
+                    csrfmiddlewaretoken: token
+                }
+            ).done(function() {
+                window.location.reload();
+            }).fail(function() {
+                navigator.id.logout()
+            })
+        },
+        onlogout: function() {}
     });
 };
 window.Superlists = {
