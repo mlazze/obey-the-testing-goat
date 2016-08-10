fee3bd2bfdbddfab6294fd4954d27e7a6deb62c2
MLazz <michele.lazzeri@studenti.unimi.it>
2016-08-10 17:46:48 +0200

    Going back to chrome

---
+++ source/accounts/static/accounts/accounts.js
@@ -0,0 +1,10 @@
+var initialize = function (navigator) {
+    $('#id_login').on('click', function () {
+        navigator.id.request();
+    });
+};
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
