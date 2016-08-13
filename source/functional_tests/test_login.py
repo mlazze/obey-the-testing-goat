dcdecb060d2e6d3742d5d3ddb18b4fdb22330c66
MLazz <michele.lazzeri@studenti.unimi.it>
2016-08-13 18:10:59 +0200

    moved FT login checkers into base

--- source/functional_tests/test_login.py
+++ source/functional_tests/test_login.py
@@ -1,8 +1,8 @@
 import time
 
-from selenium.webdriver.support.ui import WebDriverWait
-
 from .base import FunctionalTest
+
+TEST_EMAIL = 'edith@mockmyid.com'
 
 
 class LoginTest(FunctionalTest):
@@ -17,15 +17,6 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('could not find window')
 
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was {}'.format(
-                element_id,
-                self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
@@ -39,7 +30,7 @@ class LoginTest(FunctionalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
@@ -56,12 +47,8 @@ class LoginTest(FunctionalTest):
         self.wait_to_be_logged_out()
         self.browser.refresh()
 
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+    def wait_to_be_logged_in(self, email=TEST_EMAIL):
+        super().wait_to_be_logged_in(email)
 
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
+    def wait_to_be_logged_out(self, email=TEST_EMAIL):
+        super().wait_to_be_logged_out(email)
