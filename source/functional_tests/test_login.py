7faaa651b6fe4ed4e7294e2ee7fe859ca5bffc08
MLazz <michele.lazzeri@studenti.unimi.it>
2016-08-13 17:52:25 +0200

    Logout handled

--- source/functional_tests/test_login.py
+++ source/functional_tests/test_login.py
@@ -1,5 +1,4 @@
 import time
-from unittest import skip
 
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -20,7 +19,11 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(
+                element_id,
+                self.browser.find_element_by_tag_name('body').text
+            )
         )
 
     def test_login_with_persona(self):
@@ -43,6 +46,22 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
+        self.wait_to_be_logged_in()
+
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        self.browser.find_element_by_id('id_logout').click()
+
+        self.wait_to_be_logged_out()
+        self.browser.refresh()
+
+    def wait_to_be_logged_in(self):
         self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('edith@mockmyid.com', navbar.text)
