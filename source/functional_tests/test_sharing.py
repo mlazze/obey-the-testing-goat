73fecb229709cd67fcf31b2b25e4d84af62f1d8e
MLazz <michele.lazzeri@studenti.unimi.it>
2016-08-15 16:11:02 +0200

    Create Page objects for Home and List pages, use in sharing FT

--- source/functional_tests/test_sharing.py
+++ source/functional_tests/test_sharing.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 
 from functional_tests.base import FunctionalTest
+from functional_tests.home_and_list_pages import HomePage
 
 
 def quit_if_possible(browser):
@@ -23,13 +24,23 @@ class SharingTest(FunctionalTest):
         self.create_pre_authenticated_session('oniciferous@example.com')
 
         self.browser = edith_browser
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Get help\n')
+        list_page = HomePage(self).start_new_list('Get help')
 
-        share_box = self.browser.find_element_by_css_selector('input[name=email]')
+        share_box = list_page.get_share_box()
         self.assertEqual(share_box.get_attribute('placeholder'), 'your-friend@example.com')
-        
 
+        list_page.share_list_with('oniciferous@example.com')
 
+        self.browser = oni_browser
 
+        HomePage(self).go_to_home_page().go_to_my_lists_page()
 
+        self.browser.find_element_by_link_text('Get help').click()
+
+        self.wait_for(lambda: self.assertEqual(list_page.get_list_owner(), 'edith@example.com'))
+        list_page.add_new_item('Hi Edith!')
+
+        self.browser = edith_browser
+        self.browser.refresh()
+
+        list_page.wait_for_new_item_in_list('Hi Edith!', 2)
